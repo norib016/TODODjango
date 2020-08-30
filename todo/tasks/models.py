e755a33805d86754c7d47fa39b0a4e919b0412a4
Sree Bhanu Nori <47286635+norib016@users.noreply.github.com>
2020-08-30 17:15:54 -0400

    Update models.py

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
+class Task(models.Model):                                   #The class Task model would create the database 
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)       ##title and complete are fields of the model.
+                                                            ##Each field is specified as a class attribute, and each attribute maps to a database column.
 
     def __str__(self):
         return self.title
