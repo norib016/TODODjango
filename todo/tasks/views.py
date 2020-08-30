1379cf47d80452bb5988cc7d9398c86c1195aef2
Sree Bhanu Nori <47286635+norib016@users.noreply.github.com>
2020-08-30 17:51:25 -0400

    Update views.py

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -6,22 +6,22 @@ from .forms import *
 
 # Create your views here.
 
-def index(request):
-    tasks = Task.objects.all()
+def index(request):             ##index function takes an HTTPrequest object as its first parameter. Here parameter is request
+    tasks = Task.objects.all()  ##All the objects
 
     form = TaskForm()
 
     if request.method =='POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
-        return redirect('/')        
+            form.save()             ##Saving the form after a task is given
+        return redirect('/')        ##To return to the TODOlist
 
     context = {'tasks' :tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
 
 
-def updateTask(request, pk):
+def updateTask(request, pk):       ##Lets us update the task
     task = Task.objects.get(id=pk)
 
     form = TaskForm(instance=task)
@@ -30,13 +30,13 @@ def updateTask(request, pk):
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('/')        ##To return to the TODOlist
+            return redirect('/')        
 
     context = {'form': form}
     
-    return render(request, 'tasks/update_task.html', context)  ##To update a task
+    return render(request, 'tasks/update_task.html', context)  ##render it to update_task.html
 
-def deleteTask(request, pk):
+def deleteTask(request, pk):        ##Lets us delete the task
     item = Task.objects.get(id=pk)
 
     if request.method == 'POST' :
@@ -44,4 +44,4 @@ def deleteTask(request, pk):
         return redirect('/')
 
     context = {'item':item}
-    return render(request, 'tasks/delete.html', context)    ##To delete a task
+    return render(request, 'tasks/delete.html', context)    ##render it to delete.html
