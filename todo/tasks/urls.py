fdc75a51de15f80839fdfafa2b8d3c17a9217971
Sree Bhanu Nori <47286635+norib016@users.noreply.github.com>
2020-08-30 17:40:09 -0400

    Update urls.py

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="list"),
-    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-    path('delete/<str:pk>/', views.deleteTask, name="delete"),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),  ##Linking the path to update a task with update_task template
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),            ##Linking the path to delete a task with delete_task template
 
 ]
