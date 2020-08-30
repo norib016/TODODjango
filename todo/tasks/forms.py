33fd63d65dab56d19cf1ae942556f919a1f54a05
Sree Bhanu Nori <47286635+norib016@users.noreply.github.com>
2020-08-30 17:10:11 -0400

    Update forms.py

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -4,8 +4,8 @@ from django.forms import ModelForm
 from .models import *
 
 
-class TaskForm(forms.ModelForm):
+class TaskForm(forms.ModelForm):     ##TaskForm is the name of our form and modelform is what django works with 
 
     class Meta:
-        model = Task
-        fields = '__all__'
+        model = Task                ##we tell django which model should we use
+        fields = '__all__'          ##fields should end up in our form
